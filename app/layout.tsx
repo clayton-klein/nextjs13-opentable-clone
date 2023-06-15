5548c518a1f48e0fac2031c29c14a7ca95348917
clayton-klein <csklein87@hotmail.com>
2023-06-15 13:44:32 -0300

    style: create root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,33 @@
-import './globals.css'
+import { Metadata } from "next";
+import NavBar from "./components/NavBar";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "OpenTable",
+
+  // TODO: check why favicon is not working
+  icons: {
+    icon: "public/favicon.ico",
+    shortcut: "public/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="w-screen min-h-screen bg-gray-100">
+          <main className="m-auto bg-white max-w-screen-2xl">
+            <NavBar />
+            {children}
+          </main>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
