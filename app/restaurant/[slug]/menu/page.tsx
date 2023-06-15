0c6883ab6f6b0e45cbad86cff8b6ca4cab8fcf46
clayton-klein <csklein87@hotmail.com>
2023-06-15 19:48:01 -0300

    refactor: pass data dynamically  to navbar & menu

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,11 +1,37 @@
+import { PrismaClient } from "@prisma/client";
 import Menu from "../components/Menu";
 import RestaurantNavBar from "../components/RestaurantNavBar";
 
-export default function RestaurantMenu() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantMenu = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      items: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant.items;
+};
+
+export default async function RestaurantMenu({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const menu = await fetchRestaurantMenu(params.slug);
+
   return (
     <div className="w-full p-3 bg-white rounded shadow">
-      <RestaurantNavBar />
-      <Menu />
+      <RestaurantNavBar slug={params.slug} />
+      <Menu menu={menu} />
     </div>
   );
 }
