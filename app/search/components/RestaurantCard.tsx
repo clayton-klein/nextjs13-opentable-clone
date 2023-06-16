a8e966eb95172105f17fa56fd088e73a3a21ffad
clayton-klein <csklein87@hotmail.com>
2023-06-16 19:31:00 -0300

    refactor: populate card with dynamic data

--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -1,28 +1,43 @@
+import { Cuisine, Location, PRICE } from "@prisma/client";
 import Link from "next/link";
+import Price from "../../components/Price";
 
-export default function RestaurantCard() {
+interface Restaurant {
+  id: number;
+  name: string;
+  main_image: string;
+  slug: string;
+  // the types bellow were imported from Prisma
+  price: PRICE;
+  cuisine: Cuisine;
+  location: Location;
+}
+
+export default function RestaurantCard({
+  restaurant,
+}: {
+  restaurant: Restaurant;
+}) {
   return (
-    <div className="flex pb-5 border-b">
-      <img
-        src="https://resizer.otstatic.com/v2/photos/xlarge/2/29359077.webp"
-        alt=""
-        className="rounded w-44"
-      />
+    <div className="flex pb-5 border-b ml-16">
+      <img src={restaurant.main_image} alt="" className="rounded h-36 w-44" />
       <div className="pl-5">
-        <h3 className="text-3xl">Taylor and Clay</h3>
+        <h3 className="text-3xl">{restaurant.name}</h3>
         <div className="flex items-start">
           <div className="flex mb-2">*****</div>
           <p className="ml-2 text-sm">Awesome</p>
         </div>
         <div className="mb-9">
           <div className="flex text-base font-light">
-            <p className="mr-4">$</p>
-            <p className="mr-4">Brazilian</p>
-            <p className="mr-4">Sao Paulo</p>
+            <Price price={restaurant.price} />
+            <p className="mr-4 capitalize">{restaurant.cuisine.name}</p>
+            <p className="mr-4 capitalize">{restaurant.location.name}</p>
           </div>
         </div>
         <div className="text-red-600">
-          <Link href="/restaurant/milestones-grill">View more information</Link>
+          <Link href={`/restaurant/${restaurant.slug}`}>
+            View more information
+          </Link>
         </div>
       </div>
     </div>
