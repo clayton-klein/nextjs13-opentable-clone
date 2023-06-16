bf1756eff369e93f7972e39fbd641e79bce0dcd1
clayton-klein <csklein87@hotmail.com>
2023-06-16 19:32:23 -0300

    refactor: populate sidebar with dynamic data

--- app/search/components/SearchSideBar.tsx
+++ app/search/components/SearchSideBar.tsx
@@ -1,30 +1,96 @@
-export default function SearchSideBar() {
+import { Cuisine, Location, PRICE } from "@prisma/client";
+import Link from "next/link";
+
+export default function SearchSideBar({
+  locations,
+  cuisines,
+  searchParams,
+}: {
+  locations: Location[];
+  cuisines: Cuisine[];
+  searchParams: { city?: string; cuisine?: string; price?: PRICE };
+}) {
+  // just so we don't need to create 3 pretty similar Link elements (line 76)
+  const prices = [
+    {
+      id: 1,
+      price: PRICE.CHEAP,
+      label: "$",
+      className: "w-full p-2 text-base text-center font-light border rounded-l",
+    },
+    {
+      id: 2,
+      price: PRICE.REGULAR,
+      label: "$$",
+      className: "w-full p-2 text-base text-center font-light border",
+    },
+    {
+      id: 3,
+      price: PRICE.EXPENSIVE,
+      label: "$$$",
+      className: "w-full p-2 text-base text-center font-light border rounded-r",
+    },
+  ];
+
   return (
     <div className="w-1/5">
-      <div className="pb-4 border-b">
+      <div className="pb-4 border-b flex flex-col">
         <h1 className="mb-2">Region</h1>
-        <p className="text-base font-light">Toronto</p>
-        <p className="text-base font-light">London</p>
-        <p className="text-base font-light">Sao Paulo</p>
-        <p className="text-base font-light">Oakland</p>
-        <p className="text-base font-light">Sidney</p>
+        {locations.map((location) => (
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                // spread the existing query params so we don't lose them by just replacing them.
+                ...searchParams,
+                city: location.name,
+              },
+            }}
+            className="text-base font-light capitalize"
+            key={location.id}
+          >
+            {location.name}
+          </Link>
+        ))}
       </div>
-      <div className="pb-4 mt-3 border-b">
+      <div className="pb-4 mt-3 border-b flex flex-col">
         <h1 className="mb-2">Cuisine</h1>
-        <p className="text-base font-light">Brazilian</p>
-        <p className="text-base font-light">Mexican</p>
-        <p className="text-base font-light">Italian</p>
+        {cuisines.map((cuisine) => (
+          <Link
+            href={{
+              pathname: "/search",
+              query: {
+                // spread the existing query params so we don't lose them by just replacing them.
+                ...searchParams,
+                cuisine: cuisine.name,
+              },
+            }}
+            className="text-base font-light capitalize"
+            key={cuisine.id}
+          >
+            {cuisine.name}
+          </Link>
+        ))}
       </div>
       <div className="pb-4 mt-3">
         <h1 className="mb-2">Price</h1>
         <div className="flex gap-1">
-          <button className="w-full p-2 text-base font-light border rounded-l">
-            $
-          </button>
-          <button className="w-full p-2 text-base font-light border">$$</button>
-          <button className="w-full p-2 text-base font-light border rounded-r">
-            $$$
-          </button>
+          {prices.map(({ id, price, label, className }) => (
+            <Link
+              key={id}
+              href={{
+                pathname: "/search",
+                query: {
+                  // spread the existing query params so we don't lose them by just replacing them.
+                  ...searchParams,
+                  price,
+                },
+              }}
+              className={className}
+            >
+              {label}
+            </Link>
+          ))}
         </div>
       </div>
     </div>
