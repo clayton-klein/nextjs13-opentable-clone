56a419bab45abde22d0c4b017372b20945b18d98
clayton-klein <csklein87@hotmail.com>
2023-06-23 19:25:55 -0300

    feat: add authentication context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import NavBar from "./components/NavBar";
 import "./globals.css";
+import AuthContext from "./context/AuthContext";
 
 export const metadata: Metadata = {
   title: "OpenTable",
@@ -22,10 +23,26 @@ export default function RootLayout({
       <head />
       <body>
         <div className="w-screen min-h-screen bg-gray-100">
-          <main className="m-auto bg-white max-w-screen-2xl">
-            <NavBar />
-            {children}
-          </main>
+          {/**
+           * now all the components (including server components) within our app
+           * have access to the authState and setAuthState defined in this context
+           * component.
+           *
+           * this context component is a client component and remember that server
+           * components can have client components, but client components can't have
+           * server components UNLESS they are passed as children and that is what's
+           * happening here.
+           *
+           * any time we want to access state/data from the context we must use
+           * client components, we can't do it with server components, because they
+           * have no ideia about what's happening in the client.
+           */}
+          <AuthContext>
+            <main className="m-auto bg-white max-w-screen-2xl">
+              <NavBar />
+              {children}
+            </main>
+          </AuthContext>
         </div>
       </body>
     </html>
